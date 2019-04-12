2e1b620853612d0eabfe48e8ce34e55f1df9d685
Osborn <jc7483@nyu.edu>
2019-04-12 13:39:52 -0400

    ready for testing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('./db.js');
 const mongoose = require('mongoose');
 const express = require('express');
+const app = express();
 
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
 let dbconf;
@@ -16,9 +17,19 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
  // conenction string appropriately!
  const conf = JSON.parse(data);
  dbconf = conf.dbconf;
+ console.log(dbconf);
 } else {
  // if we're not in PRODUCTION mode, then use
  dbconf = 'mongodb://localhost/jc7483';
 }
+const path = require('path');
+
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect(dbconf);
+
+app.listen(process.env.PORT || 3000);
