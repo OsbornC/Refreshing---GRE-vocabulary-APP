d98006f7eaf3f5a29c5b9aa3cfacab0d378a30bc
Jieyu Chen <jc7483@linserv1.cims.nyu.edu>
2019-04-19 23:59:54 -0400

    milestone 2

--- src/app.js
+++ src/app.js
@@ -2,24 +2,24 @@ require('./db.js');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const path = require('path');
 const passport = require('passport');
 app.set('view engine', 'hbs');
 
 let dbconf;
 if (process.env.NODE_ENV === 'PRODUCTION') {
 	const fs = require('fs');
-	const path = require('path');
 	const fn = path.join(__dirname, 'config.json');
 	const data = fs.readFileSync(fn);
-
 	const conf = JSON.parse(data);
 	dbconf = conf.dbconf;
 	console.log(dbconf);
 } else {
 	dbconf = 'mongodb://localhost/jc7483';
 }
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, "..", "public")));
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -68,4 +68,4 @@ app.get('/dictionary', (req, res)=>{
 	});
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
