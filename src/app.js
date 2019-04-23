30bc3563124e56a180449b95b1555c5bf83a2bd1
Osborn <jc7483@nyu.edu>
2019-04-22 21:05:25 -0400

    almost finished quiz form

--- src/app.js
+++ src/app.js
@@ -3,26 +3,32 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const passport = require('passport');
+const Handlebars = require('hbs');
 app.set('view engine', 'hbs');
+const path = require('path');
 
-let dbconf;
-if (process.env.NODE_ENV === 'PRODUCTION') {
+// let dbconf;
+// if (process.env.NODE_ENV === 'PRODUCTION') {
 	const fs = require('fs');
-	const path = require('path');
+	
 	const fn = path.join(__dirname, 'config.json');
 	const data = fs.readFileSync(fn);
 
 	const conf = JSON.parse(data);
 	dbconf = conf.dbconf;
 	console.log(dbconf);
-} else {
+// } else {
 	dbconf = 'mongodb://localhost/jc7483';
-}
+// }
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
+
+Handlebars.registerHelper("counter", function (index){
+    return index + 1;
+});
 
 const Modules = mongoose.model('Modules');
 const Vocabulary = mongoose.model('Vocabulary');
@@ -37,21 +43,102 @@ app.get('/', (req, res)=>{
 app.post('/', (req, res)=>{
 	const username = req.body.username;
 	const password = req.body.password;
-})
+});
+
+function sampleTwo(array) {
+	const first = array[Math.floor(Math.random() * array.length )];
+	let second = array[Math.floor(Math.random() * array.length )];
+	while (first === second)
+		second = array[Math.floor(Math.random() * array.length )];
+	return [first, second];
+}
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+  return array;
+}
 
 app.post('/dictionary', (req, res)=>{
 	const word = req.body.word;
 	const meaning = req.body.meaning;
-	const vocab = new Vocabulary({
-		word: word,
-		meaning: meaning,
-		correctness: false
-	});
-	vocab.save((err, foundVocab) => {
-		if(err){
-			res.render('dictionary', {word: word, meaning: meaning, breakAndMeaning: 'Meaning:'});
+	const confusionList = [
+		'about to happen',
+		'unchangeable',
+		'unforgiving',
+		'bad judgment',
+		'owing gratitude to someone or something',
+		'to conclude from implicit evidence (as opposed to explicit facts)',
+		'naïve or innocent',
+		'harmful or hostile',
+		'tight-knit and isolated; uninterested in matters outside one’s immediate sphere',
+		'concentrated and in-depth',
+		'a go-between',
+		'indestructible, impervious to harm',
+		'irritable, testy, touchy',
+		'wishy-washy, hesitant',
+		'taciturn, reserved, succinct'
+	];
+	let confusion = sampleTwo(confusionList);
+	while (confusion[0] === req.body.meaning || confusion[1] === req.body.meaning){
+		if (confusion[0] === req.body.meaning)
+			confusion[0] = array[Math.floor(Math.random() * confusionList.length )];
+		else
+			confusion[1] = array[Math.floor(Math.random() * confusionList.length )];
+	}
+	confusion.push(req.body.meaning);
+
+	Vocabulary.count({}, function(err, c){
+		if (c < 15){
+			const voc = new Vocabulary({
+				word: word,
+				meaning: meaning,
+				correctness: false,
+				moduleID: 'module1',
+				confusion: shuffle(confusion)
+			});
+			Modules.findOneAndUpdate(
+				{moduleID: 'module1'},
+				{$push: {vocabulary: voc}},   
+				{upsert: true },
+				function(err){
+					if (err)
+						console.log(err);
+				} 
+			);
+			voc.save((err, foundVocab) => {
+				if(err){
+					res.render('dictionary', {word: word, meaning: meaning, breakAndMeaning: 'Meaning:'});
+				}else{
+					res.render('dictionary', {word: word, meaning: meaning, breakAndMeaning: 'Meaning:'});
+				}
+			});
 		}else{
-			res.render('dictionary', {word: word, meaning: meaning, breakAndMeaning: 'Meaning:'});
+			let pickedConfusion = result.map((ele)=>ele.meaning);
+			pickedConfusion.push(req.body.meaning);
+			console.log(shuffle(pickedConfusion));
+			const voc = new Vocabulary({
+				word: word,
+				meaning: meaning,
+				correctness: false,
+				moduleID: 'module1',
+				confusion: shuffle(pickedConfusion)
+			});
+			Modules.findOneAndUpdate(
+				{moduleID: 'module1'},
+				{$push: {vocabulary: voc}},   
+				{upsert: true },
+				function(err){
+					if (err)
+						console.log(err);
+				}
+			);
+			voc.save((err, foundVocab) => {
+				if(err){
+					res.render('dictionary', {word: word, meaning: meaning, breakAndMeaning: 'Meaning:'});
+				}else{
+					res.render('dictionary', {word: word, meaning: meaning, breakAndMeaning: 'Meaning:'});
+				}
+			});
 		}
 	});
 });
@@ -66,6 +153,42 @@ app.get('/dictionary', (req, res)=>{
 			res.render('dictionary', {vocabulary: foundVocab});
 		}
 	});
+});
+
+app.get('/quiz', (req, res)=>{
+	res.render('quiz');
 })
 
+app.post('/quiz', (req, res)=>{
+	Modules.find({moduleID: 'module1'}).exec((err, result) =>{
+		if (err){
+			console.log(err);
+		}else{
+			let countTrue = 0;
+			const answers = new Object();
+			const quizWords = result[0].vocabulary.map(ele => ele.word);
+			const quizMeanings = result[0].vocabulary.map(ele => ele.meaning);
+			for (let i = 0; i < quizWords.length; i++){
+				answers[quizWords[i]] = quizMeanings[i];
+				if (quizMeanings[i] === req.body[quizWords[i]]){
+					countTrue += 1;
+				}
+			}
+			const total = quizWords.length;
+			res.render('quiz', {score: 'Accuracy: ' + countTrue/total*100 + '%'});
+		}
+	});
+});	
+
+app.get('/module1', (req, res)=>{
+	Vocabulary.find().exec((err, result) => {
+		if (err){
+			console.log('Something Wrong!');
+		}else{
+			res.render('module1', {vocabulary: result});
+		}
+	});
+});
+
 app.listen(3000);
+// app.listen(process.env.PORT || 3000);
--- src/db.js
+++ src/db.js
@@ -4,25 +4,24 @@ const URLSlugs = require('mongoose-url-slugs');
 
 const User = new mongoose.Schema({
   username: {type: String, required: true},
-  password: {type: String, required: true},
-  ts:[{type: mongoose.Schema.Types.ObjectId, ref: 'Modules'}]
+  password: {type: String, required: true}
 });
 
 const Vocabulary = new mongoose.Schema({
   word: {type: String, required: true},
   meaning: {type: String, required: true},
   correctness: {type: Boolean, default: false, required: true},
-  sentence: {type: String, default: false}
+  confusion: [String],
+  moduleID: String
 });
 
 const Modules = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-  name: {type: String, required: true},
-  portion: {type: Number, required: true, default: 0, min: 0, max: 1},
+  portion: {type: Number, default: 0, min: 0, max: 1},
+  moduleID: String,
   vocabulary: [Vocabulary]
 });
 
-Vocabulary.plugin(URLSlugs('word meaning'));
+// Vocabulary.plugin(URLSlugs('word meaning'));
 
 mongoose.model('User', User);
 
